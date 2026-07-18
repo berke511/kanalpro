07e8ecc5286f926adf6eda7cc293d65563bbe138
berke511 <berke.kucuk@outlook.de>
2026-07-18 10:08:30 +0200

    Update Table.jsx

--- components/ui/v2/Table.jsx
+++ components/ui/v2/Table.jsx
@@ -1,1 +1,32 @@
+'use client';
 
+function Table({ children, className = '', ...props }) {
+  return (
+    <div className={'w-full overflow-x-auto rounded-xl border border-gray-100 ' + className}>
+      <table className="w-full border-collapse text-sm" {...props}>{children}</table>
+    </div>
+  );
+}
+function TableHead({ children, className = '', ...props }) {
+  return <thead className={'bg-gray-50 ' + className} {...props}>{children}</thead>;
+}
+function TableBody({ children, className = '', ...props }) {
+  return <tbody className={'divide-y divide-gray-100 ' + className} {...props}>{children}</tbody>;
+}
+function TableRow({ children, className = '', ...props }) {
+  return <tr className={'transition-colors hover:bg-gray-50 ' + className} {...props}>{children}</tr>;
+}
+function TableHeaderCell({ children, className = '', ...props }) {
+  return <th className={'px-4 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500 ' + className} {...props}>{children}</th>;
+}
+function TableCell({ children, className = '', ...props }) {
+  return <td className={'px-4 py-3 text-gray-700 ' + className} {...props}>{children}</td>;
+}
+
+Table.Head = TableHead;
+Table.Body = TableBody;
+Table.Row = TableRow;
+Table.Cell = TableCell;
+Table.HeaderCell = TableHeaderCell;
+
+export default Table;
